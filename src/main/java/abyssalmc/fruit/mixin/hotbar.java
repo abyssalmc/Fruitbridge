5867c02e3a2a421571bba8c940ae5f2288ad1389
BuildTools <unconfigured@null.spigotmc.org>
2024-12-09 23:28:08 +1100

    extended indicators to selector

--- src/main/java/abyssalmc/fruit/mixin/hotbar.java
+++ src/main/java/abyssalmc/fruit/mixin/hotbar.java
@@ -8,6 +8,8 @@ import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.Drawable;
 import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.client.render.RenderTickCounter;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -18,15 +20,33 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 public class hotbar {
 
+
+
     @Inject(method = "renderHotbar", at = @At(value = "HEAD"), cancellable = true)
     private void modifyHotbarTexture(DrawContext context, RenderTickCounter tickCounter, CallbackInfo ci) {
         Identifier indicator = Identifier.of(Fruit.MOD_ID, "textures/indicator.png");
+        Identifier itas = Identifier.of(Fruit.MOD_ID, "textures/itas.png");
+
 
         RenderSystem.enableBlend();
 
-        if (Fruit.isTas) { context.drawTexture(indicator, 227, 266, 0, 0, 1, 1, 1, 1); }
-        if (Fruit.isAhk) { context.drawTexture(indicator, 227, 265, 0, 0, 1, 1, 1, 1); }
-        if (Fruit.isMmt) { context.drawTexture(indicator, 227, 264, 0, 0, 1, 1, 1, 1); }
+        PlayerEntity p = MinecraftClient.getInstance().player;
+
+        if (p != null) {
+            if (Fruit.isTas) {
+                context.drawTexture(itas, context.getScaledWindowWidth() / 2 - 72 - 1 + p.getInventory().selectedSlot * 20, context.getScaledWindowHeight() - 2 - 1, 0, 0, 1, 1, 1, 1);
+                context.drawTexture(indicator, 227, 266, 0, 0, 1, 1, 1, 1);
+            }
+            if (Fruit.isAhk) {
+                context.drawTexture(itas, context.getScaledWindowWidth() / 2 - 71 - 1 + p.getInventory().selectedSlot * 20, context.getScaledWindowHeight() - 2 - 1, 0, 0, 1, 1, 1, 1);
+                context.drawTexture(indicator, 227, 265, 0, 0, 1, 1, 1, 1);
+            }
+            if (Fruit.isMmt) {
+                context.drawTexture(itas, context.getScaledWindowWidth() / 2 - 71 - 1 + p.getInventory().selectedSlot * 20, context.getScaledWindowHeight() - 3 - 1, 0, 0, 1, 1, 1, 1);
+                context.drawTexture(indicator, 227, 264, 0, 0, 1, 1, 1, 1);
+            }
+        }
+
 
         RenderSystem.disableBlend();
 
